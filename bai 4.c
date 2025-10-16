d67126dc833f3c84233846dc4158aa1689046651
ngminhduc1412007-dotcom <ngminhduc1412007@gmail.com>
2025-10-16 18:52:23 +0700

    Add files via upload

---
+++ bai 4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int height = 100;
+	int width = 50;
+	int chuvi = ( height + width ) * 2;
+	int dientich = height * width;
+	
+	printf ("chu vi hcn la %d \n", chuvi);
+	printf (" dien tich hcn la %d", dientich);
+	return 0;
+}
